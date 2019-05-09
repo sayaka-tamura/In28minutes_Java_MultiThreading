db3cfdbceb7fb245c7a5361a05835557b5eb5a55
sayaka-tamura <arccycle@gmail.com>
2019-05-09 16:17:11 +0900

    Add more 4 classes

---
+++ src/com/in28minutes/multithreading/MultipleCallableRunner.java
@@ -0,0 +1,31 @@
+package com.in28minutes.multithreading;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class MultipleCallableRunner {
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+		List<CallableTask> tasks = List.of(new CallableTask("in28Minutes"),
+
+				new CallableTask("Ranga"), new CallableTask("Adam"));
+
+		List<Future<String>> results = executorService.invokeAll(tasks);
+
+		for (Future<String> result : results) {
+
+			System.out.println(result.get());
+
+		}
+
+		executorService.shutdown();
+
+	}
+
+}
